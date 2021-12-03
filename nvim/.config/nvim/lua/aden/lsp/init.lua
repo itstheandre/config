f5ab7feb0e486287f9dcb54faeea34fa2c863791
André de Albuquerque <itstheandre@gmail.com>
2021-12-03 17:51:39 +0100

    Time to debug

--- nvim/.config/nvim/lua/aden/lsp/init.lua
+++
@@ -1,3 +0,0 @@
-require('aden/lsp/cmp')
-require('aden/lsp/diagnostic_signs')
-require('aden/lsp/language_servers')
---
+++ nvim/.config/nvim/lua/andre/lsp/init.lua
@@ -0,0 +1,3 @@
+require('andre/lsp/cmp')
+require('andre/lsp/diagnostic_signs')
+require('andre/lsp/language_servers')
